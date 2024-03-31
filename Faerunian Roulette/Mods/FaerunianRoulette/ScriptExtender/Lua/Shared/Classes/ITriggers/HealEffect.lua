93b634adeaa6166c530f28676386764c2711506b
Atílio <27153776+AtilioA@users.noreply.github.com>
2024-03-31 18:35:08 -0300

    refactor: change heal effect implementation; couldn't find a way to properly heal

--- Faerunian Roulette/Mods/FaerunianRoulette/ScriptExtender/Lua/Shared/Classes/ITriggers/HealEffect.lua
+++ Faerunian Roulette/Mods/FaerunianRoulette/ScriptExtender/Lua/Shared/Classes/ITriggers/HealEffect.lua
@@ -1,12 +1,14 @@
 Effects.HealEffect = Effects.ImplementEffect(ITriggerEffect, {
   apply = function(character)
     local currentHP = Osi.GetHitpoints(character)
-    local maxHP = Osi.CharacterGetHitpointsMax(character)
-    local maxHeal = math.floor(maxHP * Config:getCfg().FEATURES.trigger_effect.heal.max_percentage / 100)
-    local heal = math.random(0, maxHeal)
+    -- local currentHPPercentage = Osi.GetHitpointsPercentage(character)
+    -- local maxHP = (currentHP / currentHPPercentage) * 100
+    -- local maxHeal = math.floor(maxHP * Config:getCfg().FEATURES.trigger_effect.heal.max_percentage / 100)
+    -- local heal = math.random(0, maxHeal)
 
-    Osi.SetHitpoints(character, currentHP + heal)
+    Osi.ApplyStatus(character, "HEAL", 1, 100)
+    -- Osi.SetHitpoints(character, currentHP + heal)
 
-    FRPrint(1, character.name .. " has been healed for " .. heal .. " hit points.")
+    FRPrint(1, character .. " has been healed")
   end
 })
