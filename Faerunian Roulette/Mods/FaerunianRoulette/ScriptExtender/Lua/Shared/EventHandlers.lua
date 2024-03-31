1cd1f2b72661d3f77cb3b0a075a9515ac16167e1
Atílio <27153776+AtilioA@users.noreply.github.com>
2024-03-31 17:41:17 -0300

    refactor: tweak config file

--- Faerunian Roulette/Mods/FaerunianRoulette/ScriptExtender/Lua/Shared/EventHandlers.lua
+++ Faerunian Roulette/Mods/FaerunianRoulette/ScriptExtender/Lua/Shared/EventHandlers.lua
@@ -2,17 +2,22 @@ EHandlers = {}
 
 
 function EHandlers.OnTurnEnded(object)
-  FRDebug(2, "TurnEnded event fired: " .. object)
-  RouletteGame:SetCharacter(object)
+  if VCHelpers.Object:IsCharacter(object) then
+    FRDebug(2, "TurnEnded event fired: " .. object)
+    RouletteGame:SetCharacter(object)
 
-  -- Handle the 'pull_trigger_condition.turn_end' feature
-  if Config:getCfg().FEATURES.pull_trigger_condition.turn_end and VCHelpers.Object:IsCharacter(object) then
-    RouletteGame:PullTrigger()
-  end
+    -- Handle the 'pull_trigger_condition.turn_end' feature
+    if Config:getCfg().FEATURES.pull_trigger_condition.turn_end and VCHelpers.Object:IsCharacter(object) then
+      RouletteGame:PullTrigger()
+    end
 
-  -- Cleanup the 'pull_trigger_condition.random' feature if it is enabled to make sure the timer does not fire outside of the current turn
-  if Config:getCfg().FEATURES.pull_trigger_condition.random.enabled then
-    Osi.TimerCancel("FR_RandomTriggerTimer")
+    -- Cleanup the 'pull_trigger_condition.random_time' feature if it is enabled to make sure the timer does not fire outside of the current turn
+    if Config:getCfg().FEATURES.pull_trigger_condition.random_time.enabled then
+      if object == RouletteGame.character then
+        FRWarn(2, "Canceled random trigger timer.")
+        Osi.TimerCancel("FR_RandomTriggerTimer")
+      end
+    end
   end
 end
 
@@ -25,19 +30,20 @@ function EHandlers.OnTurnStarted(object)
     RouletteGame:PullTrigger()
   end
 
-  -- Handle the 'pull_trigger_condition.random' feature
-  if Config:getCfg().FEATURES.pull_trigger_condition.random.enabled then
-    local minSeconds = Config:getCfg().FEATURES.pull_trigger_condition.random.min_seconds
-    local maxSeconds = Config:getCfg().FEATURES.pull_trigger_condition.random.max_seconds
+  -- Handle the 'pull_trigger_condition.random_time' feature
+  if Config:getCfg().FEATURES.pull_trigger_condition.random_time.enabled then
+    local minSeconds = Config:getCfg().FEATURES.pull_trigger_condition.random_time.min_seconds
+    local maxSeconds = Config:getCfg().FEATURES.pull_trigger_condition.random_time.max_seconds
     local randomMS = Ext.Math.Random(minSeconds, maxSeconds) * 1000
+    FRWarn(2, "Random trigger timer set to " .. randomMS .. "ms.")
 
     Osi.TimerLaunch("FR_RandomTriggerTimer", randomMS)
   end
 end
 
 function EHandlers.OnTimerFinished(timer)
-  -- Handle the 'pull_trigger_condition.random' feature (timer has finished)
-  if timer == "FR_RandomTriggerTimer" and Config:getCfg().FEATURES.pull_trigger_condition.random.enabled then
+  -- Handle the 'pull_trigger_condition.random_time' feature (timer has finished)
+  if timer == "FR_RandomTriggerTimer" and Config:getCfg().FEATURES.pull_trigger_condition.random_time.enabled then
     FRDebug(2, "TimerFinished event fired: " .. timer)
     RouletteGame:PullTrigger()
   end
--- Faerunian Roulette/Mods/FaerunianRoulette/ScriptExtender/Lua/Shared/Helpers/Config.lua
+++ Faerunian Roulette/Mods/FaerunianRoulette/ScriptExtender/Lua/Shared/Helpers/Config.lua
@@ -6,65 +6,46 @@ Config = VCHelpers.Config:New({
       enabled = true, -- Toggle the mod on/off
     },
     FEATURES = {
-      trigger_effect = {  -- What should happen when the 'trigger' is pulled and the 'chamber' has a 'bullet' in it. Having more than one of these enabled will make them share their chance of being triggered (e.g. 50% chance for each if two are enabled and they have the same chamber size)
+      chamber_size = 6,                                  -- Default is a six-chamber 'revolver'
+      bullets_in_chambers = 1,                           -- Default is 1, which is the same as having one 'bullet' in the 'chamber'
+      trigger_effect = {                                 -- What should happen when the 'trigger' is pulled and the 'chamber' has a 'bullet' in it. Having more than one of these enabled will make one be randomly picked, essentially sharing the probability of each effect happening.
         sound_effect = {
-          enabled = true, -- Play a sound effect
-          -- sound = "Spell_Cast_Damage_Force_EldritchBlast_L1to3",    -- Sound effect to play
-          sound = "dee8060b-c1c2-7343-db76-dd7f2cd36e24"
+          enabled = true,                                -- Play a sound effect
+          sound = "dee8060b-c1c2-7343-db76-dd7f2cd36e24" -- Sound effect to play
         },
-        kill = true,           -- Kill the character
-        downed = false,        -- Down the character
-        heal = {               -- Heal the character
+        kill = true,                                     -- Kill the character
+        heal = {                                         -- Heal the character
           enabled = false,
-          amount = 0           -- Amount of healing
+          max_percentage = 100                           -- Maximum percentage of health to heal
         },
-        damage = {             -- Damage the character
+        damage = {                                       -- Damage the character, random amount
           enabled = false,
-          amount = 0           -- Amount of damage
+          max_percentage = 100,                          -- Maximum percentage of health to damage
         },
-        status = {             -- Apply a status to the character
-          -- Osi.ApplyStatus(object, status, duration, force, source) end
-          prone = false,       -- Apply the 'Prone' status
-          stunned = false,     -- Apply the 'Stunned' status
-          frozen = false,      -- Apply the 'Frozen' status
-          burning = false,     -- Apply the 'Burning' status
-          poisoned = false,    -- Apply the 'Poisoned' status
-          blessed = false,     -- Apply the 'Blessed' status
-          charmed = false,     -- Apply the 'Charmed' status
-          frightened = false,  -- Apply the 'Frightened' status
-          paralyzed = false,   -- Apply the 'Paralyzed' status
-          petrified = false,   -- Apply the 'Petrified' status
-          silenced = false,    -- Apply the 'Silenced' status
-          sleep = false,       -- Apply the 'Sleep' status
-          blinded = false,     -- Apply the 'Blinded' status
-          cursed = false,      -- Apply the 'Cursed' status
-          diseased = false,    -- Apply the 'Diseased' status
-          invisible = false,   -- Apply the 'Invisible' status
-          polymorphed = false, -- Apply the 'Polymorphed' status
-          webbed = false,      -- Apply the 'Webbed' status
-          entangled = false,   -- Apply the 'Entangled' status
-          -- ...
+        apply_status = {                                 -- Apply a random status to the character. This includes over a hundred statuses, so expect chaos! Save often.
+          enabled = false,
+          max_duration = 2,                              -- Max duration of the status in turns. May stack statuses.
+          ignore_resistances = false                     -- Ignore resistances when applying the status
         },
         ignore = {
           bosses = true, -- Ignore bosses
         },
       },
-      chamber_size = 6,       -- Default is a six-chamber 'revolver'
-      bullets_in_chambers = 1, -- Default is 1, which is the same as having one 'bullet' in the 'chamber'
-      spin_chamber = {        -- Reset the 'revolver chamber' after a trigger, to guarantee the same effect chance every time. If disabled, the chances will increase with each trigger. https://en.wikipedia.org/wiki/Russian_roulette#Variant:_revolver_only_spun_once_at_the_start
-        enabled = true,
-        sound_effect = {
-          enabled = true,
-          sound = "VFX_TrialOfJustice_LightningOrb",
-        }
-      },
       pull_trigger_condition = { -- Conditions for pulling the trigger; they are independent of each other and can be combined
         turn_end = true,         -- Pull the trigger at the end of the character's turn
         turn_start = false,      -- Pull the trigger at the start of the character's turn
-        random = {
-          enabled = false,       -- Pull the trigger at a random time
+        random_time = {
+          enabled = false,       -- Pull the trigger at a random time during the character's turn
           min_seconds = 1,       -- Minimum number of seconds before the trigger can be pulled
           max_seconds = 60,      -- Maximum number of seconds before the trigger can be pulled
+        }
+      },
+      spin_chamber = { -- Reset the 'revolver chamber' after 'pulling the trigger', to guarantee the same effect chance every time. If disabled, the chances will increase with each 'trigger pull'. https://en.wikipedia.org/wiki/Russian_roulette#Variant:_revolver_only_spun_once_at_the_start
+        enabled = true,
+        sound_effect = {
+          enabled = true,
+          sound = "Spell_Cast_Damage_Force_EldritchBlast_L1to3"
+          -- sound = "VFX_TrialOfJustice_LightningOrb",
         }
       }
     },
